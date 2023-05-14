4991ae9535e90b69116a9db95f84db740e41dbfe
edger477 <edgingforever@outlook.com>
2023-05-14 11:00:48 +0200

    calibration improvement

--- Software/ESP32-Example/lib/WT312/WT312.cpp
+++ Software/ESP32-Example/lib/WT312/WT312.cpp
@@ -27,27 +27,29 @@ void WT312::setIntensity(uint8_t intensity){
 }
 
 int WT312::_characModule(){
-    _DAC_MAX = 255;
+    _DAC_MAX = 0;
     _DAC_MIN = 0;
 
-    uint16_t prev_shuntVoltage = 0;
+    uint16_t maxShuntVoltage = 0, minShuntVoltage = 4095;
     for(uint8_t DACcode = 0; DACcode < 255; DACcode++){
         uint16_t shuntVoltage = _testCurrent(DACcode);
         Serial.printf("DACCode: %d, Voltage: %d \n", DACcode, shuntVoltage);
-        if (shuntVoltage > prev_shuntVoltage){
+        if (shuntVoltage > maxShuntVoltage)
+        {
             _DAC_MIN = DACcode;
-            prev_shuntVoltage = shuntVoltage;
+            maxShuntVoltage = shuntVoltage;
         }
-        if(shuntVoltage < 250){
+        // I noticed weird voltages at start so delay until DACcode 20
+        if (shuntVoltage < minShuntVoltage && DACcode > 20)
+        {
             _DAC_MAX = DACcode;
-            break;
+            minShuntVoltage = shuntVoltage;
         }
     }
-    Serial.print("DAC_MIN: "); Serial.println(_DAC_MIN);
-    Serial.print("DAC_MAX: "); Serial.println(_DAC_MAX);
+    Serial.printf("DAC_MIN: %d; DAC_MAX: %d \n", _DAC_MIN, _DAC_MAX);
+
     if((_DAC_MAX - _DAC_MIN) < 90) return 0;
     return 1;
-
 }
 
 uint16_t WT312::_testCurrent(uint8_t DACCode){
